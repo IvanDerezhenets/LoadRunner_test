98c087f96968404d183e774d2eb4ef7c0cc3449d
IvanDerezhenets <71653500+IvanDerezhenets@users.noreply.github.com>
2022-03-11 23:06:55 +0300

    Update Action.c

--- Ex_02_НТ/Wishlist_2/Action.c
+++ Ex_02_НТ/Wishlist_2/Action.c
@@ -5,38 +5,8 @@ Action()
 
 	lr_start_transaction("1_begin");
 
-/*Correlation comment: Automatic rules - Do not change!  
-Original value='authentication' 
-Name ='controller' 
-Type ='Rule' 
-AppName ='vk' 
-RuleName ='3'*/
-	web_reg_save_param_regexp(
-		"ParamName=controller",
-		"RegExp=controller=(.*?)\"\\ method",
-		"Ordinal=1",
-		SEARCH_FILTERS,
-		"Scope=Body",
-		"RequestUrl=*/index.php*",
-		LAST);
-
 	web_reg_find("Search=All",
 		"Text=Already registered",
-		LAST);
-
-/*Correlation comment: Automatic rules - Do not change!  
-Original value='my-account' 
-Name ='back' 
-Type ='Rule' 
-AppName ='test1' 
-RuleName ='12'*/
-	web_reg_save_param_regexp(
-		"ParamName=back",
-		"RegExp=controller=(.*?)\"\\ title",
-		"Ordinal=36",
-		SEARCH_FILTERS,
-		"Scope=Body",
-		"RequestUrl=*/index.php*",
 		LAST);
 
 	web_url("index.php", 
@@ -56,7 +26,7 @@ RuleName ='12'*/
 /*Correlation comment - Do not change!  Original value='279ce434016d7cf72c9f127af85c4679' Name ='token' Type ='ResponseBased'*/
 	web_reg_save_param_regexp(
 		"ParamName=token",
-		"RegExp==\\ '(.*?)';\\\nvar\\ usingSecureMode\\ ",
+		"RegExp=token = '(.*?)'",
 		SEARCH_FILTERS,
 		"Scope=Body",
 		"IgnoreRedirections=Yes",
@@ -67,35 +37,19 @@ RuleName ='12'*/
 		"Text=My account",
 		LAST);
 
-/*Correlation comment: Automatic rules - Do not change!  
-Original value='mywishlist' 
-Name ='controller_1' 
-Type ='Rule' 
-AppName ='test1' 
-RuleName ='12'*/
-	web_reg_save_param_regexp(
-		"ParamName=controller_1",
-		"RegExp=controller=(.*?)\"\\ title",
-		"Ordinal=23",
-		SEARCH_FILTERS,
-		"Scope=Body",
-		"IgnoreRedirections=Yes",
-		"RequestUrl=*/index.php*",
-		LAST);
-
 	web_submit_data("index.php_2",
-		"Action=http://automationpractice.com/index.php?controller={controller}",
+		"Action=http://automationpractice.com/index.php?controller=authentication",
 		"Method=POST",
 		"EncodeAtSign=YES",
 		"TargetFrame=",
 		"RecContentType=text/html",
-		"Referer=http://automationpractice.com/index.php?controller={controller}&back={back}",
+		"Referer=http://automationpractice.com/index.php?controller=authentication&back=my-account",
 		"Snapshot=t25.inf",
 		"Mode=HTML",
 		ITEMDATA,
 		"Name=email", "Value={login}", ENDITEM,
 		"Name=passwd", "Value={password}", ENDITEM,
-		"Name=back", "Value={back}", ENDITEM,
+		"Name=back", "Value=my-account", ENDITEM,
 		"Name=SubmitLogin", "Value=", ENDITEM,
 		LAST);
 
@@ -104,11 +58,11 @@ RuleName ='12'*/
 	lr_start_transaction("3_wishlist");
 
 	web_url("My wishlists",
-		"URL=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller={controller_1}",
+		"URL=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist",
 		"TargetFrame=",
 		"Resource=0",
 		"RecContentType=text/html",
-		"Referer=http://automationpractice.com/index.php?controller={back}",
+		"Referer=http://automationpractice.com/index.php?controller=my-account",
 		"Snapshot=t26.inf",
 		"Mode=HTML",
 		LAST);
@@ -122,11 +76,11 @@ RuleName ='12'*/
 		LAST);
 
 	web_submit_data("index.php_3",
-		"Action=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller={controller_1}",
+		"Action=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist",
 		"Method=POST",
 		"TargetFrame=",
 		"RecContentType=text/html",
-		"Referer=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller={controller_1}",
+		"Referer=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist",
 		"Snapshot=t27.inf",
 		"Mode=HTML",
 		ITEMDATA,
@@ -148,7 +102,7 @@ RuleName ='12'*/
 		"TargetFrame=",
 		"Resource=0",
 		"RecContentType=text/html",
-		"Referer=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller={controller_1}",
+		"Referer=http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist",
 		"Snapshot=t28.inf",
 		"Mode=HTML",
 		LAST);
